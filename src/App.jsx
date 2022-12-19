6820dd47d1adbb2a87de92af988c79bb22de49f7
fromHeisei <68497872+fromHeisei@users.noreply.github.com>
2022-12-20 00:02:52 +0900

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
-  };
+  const onClickButton = () => alert();
   const contentStyle = {
     color: "blue",
     fontSize: "18px"
   };
   return (
     <>
-      <h1 style={{ color: "green" }}>Hello</h1>
-      <p style={contentStyle}>My mone </p>
-      <button onClick={onClickButton}>ボタン</button>
+      <h1 style={{ color: "pink" }}>Hello</h1>
+      <p style={contentStyle}>World!</p>
+      <button onClick={onClickButton}>button</button>
     </>
   );
 };
